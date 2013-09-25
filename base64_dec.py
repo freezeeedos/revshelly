c614a77e7cb4b56c4369cca0bb6b1606ad3d546d
quentin <freezeeedos@gmail.com>
2013-09-25 20:45:18 +0200

    added help

--- base64_dec.py
+++ base64_dec.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python2
 import base64, sys
 
+if len(sys.argv) < 3:
+    print '''Usage:\n    %s <input file> <output file>''' % sys.argv[0]
+    exit(0)
+
 infile = open(sys.argv[1], 'r')
 outfile = open(sys.argv[2], 'w')
 base64.decode(infile, outfile)
